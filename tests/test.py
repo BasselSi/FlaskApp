60b81d866b78aed7c03fad20e6e5136a8d1773ee
Bassel <bassel.sinokrot@gmail.com>
2025-09-30 17:29:02 +0300

    add Pytestwith updated workflow to run the tests with every push

---
+++ tests/test.py
@@ -0,0 +1,16 @@
+import pytest
+from flask import Flask
+import app as flask_app_module
+
+@pytest.fixture
+def client():
+	flask_app_module.app.config['TESTING'] = True
+	with flask_app_module.app.test_client() as client:
+		yield client
+
+def test_sum_page(client):
+	# Send dummy inputs
+	response = client.post('/', data={'x': '2', 'y': '3'})
+	assert response.status_code == 200
+	# Check that the result is shown on the page
+	assert b'Result: 5' in response.data
